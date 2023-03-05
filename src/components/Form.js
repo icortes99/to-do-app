600c3ec8b001df71553d9c5262980cd8a63f34d4
Ivan <cortes.ivan353@gmail.com>
2023-03-05 13:47:17 -0600

    UI 80%, add functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,38 +5,51 @@ import {useState} from 'react'
 
 function Form(){
     const block = 'form'
-    const someTasks = [{
+    const [tasks, setTasks] = useState([{
+        id: '433c6b39d6fc6',
         title: 'Buy groceries for next week',
         description: 'Groceries: apples, watermelon, orange',
         due_date: '2/2/22',
         created: '2/2/22',
         completed: true,
-        active: 1
+        active: true
     }, {
+        id: 'fd50c99ebac6d',
         title: 'Renew car insurance',
         description: '$60 for tires, $20 insurance and $80 for engine',
         due_date: '2/2/22',
         created: '2/2/22',
         completed: false,
-        active: 1
-    }]
-    const [tasks, setTasks] = useState([])
-    const [accordion, setAccordion] = useState(0) //state para manejar cual Task tiene la info desplegada
+        active: true
+    }])
+    //const [accordion, setAccordion] = useState(0) //state para manejar cual Task tiene la info desplegada
     const handleSubmit = (event)=>{
         event.preventDefault()
         const taskTitle = event.target.elements.taskTitle.value
         const taskDescription = event.target.elements.taskDescription.value
-        const taskDueDate = event.target.elements.taskDueDate.value
+        const taskDueDate = '3/3/03'//event.target.elements.taskDueDate.value
         const newTask = {
+            id: Math.random().toString(16).slice(2),
             title: taskTitle,
             description: taskDescription,
             due_date: taskDueDate,
             created: new Date().toLocaleDateString('es-ES').replace(/\//g, '/').slice(0, -2),
             completed: false,
-            active: 1
+            active: true
         }
         setTasks([...tasks, newTask])
         event.target.reset()
+    }
+    const deleteTask = (idTask)=>{
+        console.log('idtask' + idTask)
+        let newTasks = tasks
+        const index = newTasks.findIndex(obj => obj.id === idTask)
+        let temp = newTasks[index]
+        temp.active = false
+        //newTasks.splice(index, 1)
+        newTasks[index] = temp
+        console.log('new tasks' + JSON.stringify(newTasks))
+        setTasks(newTasks)
     }
 
     return(
@@ -46,15 +59,23 @@ function Form(){
                     <List className={`${block}__list-icon`}/>
                     <h1>My Todo-s</h1>
                 </div>
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <input placeholder='new task'/>
-                        <input placeholder='new description'/>
+                <form onSubmit={handleSubmit} className={`${block}__form`}>
+                    <div className={`${block}__form__inputs`}>
+                        <input name='taskTitle' placeholder='New task'/>
+                        <input name='taskDescription' placeholder='New description'/>
                     </div>
-                    <button type='submit'>Add task</button>
+                    <div className={`${block}__form__buttons`}>
+                        <label htmlFor='due_date'></label>
+                        <input id='due_date' type='date' className={`${block}__calendar-input`}/>
+                        <button type='submit'>ADD</button>
+                    </div>
                 </form>
-                <Task task={someTasks[0]}/>
-                <Task task={someTasks[1]}/>
+                {tasks.map((i, j)=>
+                    <Task
+                    key={j}
+                    task={i}
+                    deleteF={deleteTask}/>
+                )}
             </div>
         </div>
     )
--- src/components/Task.js
+++ src/components/Task.js
@@ -4,16 +4,16 @@ import {ReactComponent as Delete} from '../svg_icons/svg_delete.svg'
 
 function Task(props){
     const block = 'task'
-    const {task} = props
+    const {task, deleteF} = props
 
     return(
         <div className={`${block}`}>
-           <input className={`${block}__check`} type='checkbox' checked={!task.completed}/>
+           <input className={`${block}__check`} type='checkbox' defaultChecked={task.completed}/>
            <p className={`${block}__title`}>{task.title}</p>
            <div>
                 <Edit className={`${block}__svg`}/>
-                <Delete className={`${block}__svg`}/>
-                <p className={`${block}__date`}>Date</p>
+                <Delete className={`${block}__svg`} onClick={()=>deleteF(task.id)}/>
+                <p className={`${block}__date`}>{task.created}</p>
            </div>
         </div>
     )
